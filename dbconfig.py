f0e488f71d63acbbdd129c2f677fce5854cf2547
Ciaran Moran <ciaranmoran@gmail.com>
2024-04-20 21:18:51 +0100

    fixed typo in dbconfig.py

--- dbconfig.py
+++ dbconfig.py
@@ -2,7 +2,7 @@
 # This will be changed on the web server itself once hosted.
 # Avoids the real DB login info from being published on github.
 # Yet also facilitates the (dummy) file itself existing in the Github repo.
-{
+mysql = {
     'host':"localhost",
     'user':"root",
     'password':"",
