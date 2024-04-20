8fe64573fc7cdc847aa233f6de620a4c2db47e01
Ciaran Moran <ciaranmoran@gmail.com>
2024-04-20 19:38:04 +0100

    Added required viewer html, DAO, config etc files

---
+++ dbconfig.py
@@ -0,0 +1,10 @@
+# Note, this is a dummy db config file.
+# This will be changed on the web server itself once hosted.
+# Avoids the real DB login info from being published on github.
+# Yet also facilitates the (dummy) file itself existing in the Github repo.
+{
+    'host':"localhost",
+    'user':"root",
+    'password':"",
+    'database':"datarepresentation"
+}
---
+++ main.py
@@ -0,0 +1,7 @@
+# BIG PROJECT
+# by Ciaran Moran 
+# this is the main viewer python program
+#
+#
+#
+#
